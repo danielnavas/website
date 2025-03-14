544bcf34cfdf4ce4968ec10cc6e0f6542e08c6b6
Daniel Navas <danielnavas43@gmail.com>
2025-03-14 12:59:54 -0700

    updated footer.jsx

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -1,8 +1,8 @@
 export const Footer = () => {
 
 return <footer className="fixed bottom-0 w-full z-40 bg-gray backdrop-blur-lg border-t border-white/20 shadow-lg">
-    <div className="max-w-5xl mx-auto px-4 flex justify-center items-center h-16">
-        <a className="text-gray-300">
+    <div className="max-w-5xl mx-auto px-4 flex justify-center items-center h-12">
+        <a className="text-sm text-gray-400">
             &#169; Daniel Navas, 2025.
         </a>
     </div>
