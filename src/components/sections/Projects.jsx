4da57c6ba70f8c04d2e7e2cee2f49167c864b0ad
Daniel Navas <danielnavas43@gmail.com>
2025-03-26 15:03:43 -0700

    added info to projects

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -84,6 +84,31 @@ export const Projects = () => {
                         </a>
                     </div> 
                 </div>
+                <div className="p-6 rounded-xl border border-white/20 
+                    hover:-translate-y-1 hover:border-teal-500/30 hover:shadow-[0_5px_50px_rgba(0,128,128,0.3)] transition-all">
+                        <div>
+                            <img class="object-scale-down size-48 rounded-md" alt="" src="/website/battery.jpg" />
+                        </div>
+                        <h3 className="text-xl font-bold mb-2 mt-2"> Video Game Battery Replacement</h3>  
+                        <p className="text-gray-400 mb-4">Revived some old video games that had used up batteries. Goes into the different battery types,
+                             what soldering methods work best on these boards, and why some games don't actually need replacements!</p>  
+                    <div className="flex flex-wrap gap-2 mb-4">
+                        {["Soldering", "Electronics"].map((tech, key) => (
+                            <span 
+                            key={key}
+                            className="bg-teal-500/10 text-teal-500 py-1 px-3 rounded-full text-sm
+                            hover:blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                            {tech}
+                            </span>
+                        ))}    
+                    </div>
+                    <div className="flex">
+                        <a href="#" 
+                        className=" text-teal-500 hover:text-teal-400 transition-colors my-4">
+                        Full write-up coming soon!
+                        </a>
+                    </div> 
+                </div>
 
                 
 
