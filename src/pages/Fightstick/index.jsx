a5ad78fda07920823ecd2e40aca2c6b392172541
Daniel Navas <danielnavas43@gmail.com>
2025-04-07 19:27:52 -0700

    updated fightstick project page

--- src/pages/Fightstick/index.jsx
+++ src/pages/Fightstick/index.jsx
@@ -2,11 +2,10 @@ import { RevealOnScroll } from "../../components/RevealOnScroll";
 
 export const Fightstick = () => {
   
-    return <div className="flex items-center justify-center p-20 md:p-25">
-        <RevealOnScroll>
-        <div className="flex flex-col-reverse md:flex-row items-center bg-black justify-center max-w-5xl px-4 md:h-96 word-wrap:break-word text:balance">
-
-            <div className="text-center md:text-left sm:flex-col md:flex-row max-w-5xl"> 
+    return <div className="items-center justify-center p-20 md:p-25">
+        
+        <div className="flex mx-auto flex-col-reverse md:flex-row items-center justify-center max-w-5xl px-4 md:h-96 word-wrap:break-word text:balance">
+            <div className="text-center md:text-left sm:flex-col md:flex-row max-w-xl"> 
                 <h3 className=" text-3xl font-bold mb-8 bg-gradient-to-r from-teal-400 to-cyan-600
                 bg-clip-text text-transparent">Hori HPS-1 PS1 Fightstick Modification</h3>  
                 <p className=" text-gray-400 mb-4">I purchased a very, very old fightstick and used its poorly made internals as an opportunity to get
@@ -21,14 +20,18 @@ export const Fightstick = () => {
                         </span>
                     ))}    
                 </div>
+
+
             </div> 
-
-                    <div>
-                    <img className="object-scale-down md:pl-4" alt="" src="/website/fightstick.jpg"/>
-                    </div>  
-
+            <div>
+                    <img className="object-scale-down md:pl-4 size-96" alt="" src="/website/fightstick.jpg"/>
+            </div>
+            
+              
         </div> 
 
+        
+         
         <div className="text-center text-gray-400 md:text-left max-w-5xl mx-auto px-4 py-10">
             <p>The idea for this project started way back in May 2024. At the time, I thought it would be cool to have a retro arcade stick and bring it to local events, so I started to search what I wanted in a fightstick. My older one, a Hori RAP-4, was decent enough, but I found that I also wanted something with a bit more weight to it. Oftentimes I would input a motion and it would move my stick just a bit, and I hated readjusting. There are solutions on the internet where people put weights inside their stick, but I thought it'd be more interesting if I found one with a more solid chassis. Unfortunately, any fightstick made before seventh generation consoles isn't really made for competitive play, and the Hori HPS-1 for the Playstation is one of the few sticks that has a solid base to work with.                
             </p>
@@ -171,10 +174,7 @@ export const Fightstick = () => {
                         <img className="mx-auto object-scale-down object-center size-80 " alt="" src="/website/done2.jpg"/>
                 </div>
             </div>
-        </div>
-        
-            
+        </div>   
 
-        </RevealOnScroll>
     </div>;
 };
